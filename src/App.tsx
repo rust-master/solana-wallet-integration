88bd0d06f46c00dc3acaa52234547b2a209434c6
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2022-05-26 14:55:01 +0500

    register component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import { FC } from "react";
 import Context from "./components/Context/Context";
 import ConnectWallet from "./components/ConnectWallet/ConnectWallet";
-
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import Register from "./components/Register/Register";
 
 require("./App.css");
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const App: FC = () => {
     return (
-        <div>
-            <Context>
-                < div className="app">
-                    <ConnectWallet />
-                </div>
-            </Context>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={
+                        <Context>
+                            <div className="app">
+                                <ConnectWallet />
+                            </div>
+                        </Context>} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 };
 export default App;
